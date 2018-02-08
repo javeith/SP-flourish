8ceb0fdb64de26361916b34f3ff37eb513f70e3c
Jannic Veith <jveith@ethz.ch>
2018-02-08 12:29:36 +0100

    labeing function very nice, working on ipynb

--- Matlab/labelTrainingData.m
+++ Matlab/labelTrainingData.m
@@ -5,16 +5,15 @@
 %
 %% Method description
 %
-% This script labels data with labels 0 to 8. For each desired label class
-% the handle assignLabel below should be set to true. When set to true, an
-% image will open with the expected class label in the terminal. In this
-% image one will have to select a minimum of 3 points counter-clockwise, so
-% that they form a convex polygon. Finish selection with enter, remove last
-% point with backspace. All the pixels in this area with NDVI > cutoffNDVI 
-% will be set to the corresponding class label.
+% This script labels all points with NDVI >= cutoffNDVI with labels 0 to 8.
+% For Soil, Road and Background the condition is NDVI < cutoffNDVI. 
+% See Labeling for more info.
 %
+% First select the desired label in the dropdown menu, then choose the 
+% limiting corners in COUNTER-CLOCKWISE direction. Confirm selection of 
+% points with ENTER,remove last selected point with BACKSPACE.
+% 
 % Input orthomosaic paths are pathBand1 and pathBand8.
-%
 % The labeled image will be saved to the path savePath as a .mat file.
 % 
 
@@ -23,28 +22,18 @@ clc
 clear all;
 close all;
 
-% Plots on or off
-plotOn = true;
-
 % Paths for orthomosaics for Band1 (700nm) and Band8 (803nm)
-pathBand1 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170622/Orthomosaics/Band1.png';
-pathBand8 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170622/Orthomosaics/Band8.png';
+pathBand1 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/Orthomosaics/Band1.png';
+pathBand8 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/Orthomosaics/Band8.png';
 
 % Path to save the labeled data as .mat file
-savePath = '/Users/jveith/Documents/2018 Semester Project/Python/trainingLabels.mat';
+savePath = '/Volumes/mac_jannic_2017/thanujan/Datasets/labeledOrthomosaics/XimeaT20170510.mat';
 
-% Cutoff NDVI for best separation of plants and soil
-cutoffNDVI = .51;
-
-% Specify which plants appear in the image
-assignCorn =        true;
-assignSugarbeet =   true;
-assignWinterwheat = true;
-assignRoad =        false;
-assignSoil =        false;
-assignBuckwheat =   false;
-assignGrass =       false;
-assignSoybean =     false;
+% Cutoff NDVI for best separation of plants and soil. If you want to see
+% the NDVI image for point selection, set cutoffON to false. The data
+% labeling still uses the cutoffNDVI when cutoffON is false.
+cutoffNDVI = .505;
+cutoffON = false;
 
 
 %% Labeling
@@ -64,147 +53,48 @@ classNames = {'Background','Corn','Sugarbeet','Winterwheat','Road','Soil',...
 classNumbers = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 classLabel = containers.Map(classNames,classNumbers);
 
-%% Loading images and computing NDVI
+% Colors for plotting
+colors= [0,0,0;0,0,255;255,0,0;0,255,0;255,128,0;110,25,0;125,0,255;255,255,0;0,137,255];
+
+
+%% Load and initialize images
+
+% Loading images and computing NDVI
 orthomosaicBand1 = im2double(rgb2gray(imread(pathBand1)));
 orthomosaicBand8 = im2double(rgb2gray(imread(pathBand8)));
 
 % (Band8 - Band1) / (Band8 + Band1) = (803-700) / (803+700)
 NDVI = (orthomosaicBand8 - orthomosaicBand1) ./ (orthomosaicBand1 + orthomosaicBand8);
 
-% Threshholding 
-CutNDVI=NDVI;
-CutNDVI(NDVI>= cutoffNDVI) = 255;
-CutNDVI(NDVI< cutoffNDVI) = 0;
-
-%% Create labeling polygons via ordered edgepoints, counter clockwise
-
-% Points [X,Y] are corners of a convex polygon, corner points must be
-% ordered counter clockwise. Needs a minimum of 3 points.
-
-% Ximea_Tamron/20170622
-edgesCorn = [];
-edgesSugarbeet = [];
-edgesWinterwheat = [];
-edgesRoad = [];
-edgesSoil = [];
-edgesBuckwheat = [];
-edgesGrass = [];
-edgesSoybean = [];
-
-if assignCorn == true
-    disp('Waiting for corner points for Corn...');
-    [edgesCorn(:,1),edgesCorn(:,2),~] = impixel(CutNDVI);
-    disp('Corn completed.');
-end
-if assignSugarbeet == true
-    disp('Waiting for corner points for Sugarbeet...');
-    [edgesSugarbeet(:,1),edgesSugarbeet(:,2),~] = impixel(CutNDVI);
-    disp('Sugarbeet completed.');
-end
-if assignWinterwheat == true
-    disp('Waiting for corner points for Winterwheat...');
-    [edgesWinterwheat(:,1),edgesWinterwheat(:,2),~] = impixel(CutNDVI);
-    disp('Winterwheat completed.');
-end
-if assignRoad == true
-    disp('Waiting for corner points for Road...');
-    [edgesRoad(:,1),edgesRoad(:,2),~] = impixel(CutNDVI);
-    disp('Road completed.');
-end
-if assignSoil == true
-    disp('Waiting for corner points for Soil...');
-    [edgesSoil(:,1),edgesSoil(:,2),~] = impixel(CutNDVI);
-    disp('Soil completed.');
-end
-if assignBuckwheat == true
-    disp('Waiting for corner points for Buckwheat...');
-    [edgesBuckwheat(:,1),edgesBuckwheat(:,2),~] = impixel(CutNDVI);
-    disp('Buckwheat completed.');
-end
-if assignGrass == true
-    disp('Waiting for corner points for Grass...');
-    [edgesGrass(:,1),edgesGrass(:,2),~] = impixel(CutNDVI);
-    disp('Grass completed.');
-end
-if assignSoybean == true
-    disp('Waiting for corner points for Soybean...');
-    [edgesSoybean(:,1),edgesSoybean(:,2),~] = impixel(CutNDVI);
-    disp('Soybean completed.');
+% Threshholding for plotting
+CutNDVI=im2uint8(NDVI);
+if cutoffON
+    CutNDVI(NDVI>= cutoffNDVI) = 255;
+    CutNDVI(NDVI< cutoffNDVI) = 0;
 end
 
-close figure(1)
-%% Create orthomosaic containing numbers 0-8 as labels
 labeledPicture = zeros(size(NDVI,1),size(NDVI,2),1);
 
-for ii=1:size(labeledPicture,1)
-    for jj=1:size(labeledPicture,2)
-        if NDVI(ii,jj) >= cutoffNDVI 
-            if isInsidePolygon([jj,ii],edgesCorn) == true
-                labeledPicture(ii,jj)=classLabel('Corn');
-            end
-            if isInsidePolygon([jj,ii],edgesSugarbeet) == true
-                labeledPicture(ii,jj)=classLabel('Sugarbeet');
-            end
-            if isInsidePolygon([jj,ii],edgesWinterwheat) == true
-                labeledPicture(ii,jj)=classLabel('Winterwheat');
-            end
-            if isInsidePolygon([jj,ii],edgesRoad) == true
-                labeledPicture(ii,jj)=classLabel('Road');
-            end
-            if isInsidePolygon([jj,ii],edgesSoil) == true
-                labeledPicture(ii,jj)=classLabel('Soil');
-            end
-            if isInsidePolygon([jj,ii],edgesBuckwheat) == true
-                labeledPicture(ii,jj)=classLabel('Buckwheat');
-            end
-            if isInsidePolygon([jj,ii],edgesGrass) == true
-                labeledPicture(ii,jj)=classLabel('Grass');
-            end
-            if isInsidePolygon([jj,ii],edgesSoybean) == true
-                labeledPicture(ii,jj)=classLabel('Soybean');
-            end
-        end
+
+%% Keep looping for every label the user wants to set.
+
+while 1
+    colorPicture = fillColors(CutNDVI,labeledPicture,colors);
+    plotColorImage(1,colorPicture,classNames,classLabel,colors)
+    label = choosedialog(classNames);
+    if strcmp(label, 'done' )
+        break; 
     end
+    figure(2)
+    [edges(:,1),edges(:,2),~] = impixel(colorPicture);
+    labeledPicture = labelPicture(labeledPicture,edges,label,classLabel,cutoffNDVI,NDVI);
+    clear edges
+end
+if ishandle(2)
+    close(2);
 end
 
 save(savePath,'labeledPicture');
-
-%% Create labeled color image
-
-colors= [0,0,0;0,0,255;255,0,0;0,255,0;255,128,0;110,25,0;125,0,255;255,255,0;0,137,255];
-
-colorRed = CutNDVI;
-colorGreen= CutNDVI;
-colorBlue = CutNDVI;
-for ii= 1:8
-    colorRed(labeledPicture==ii) = colors(ii+1,1);
-    colorGreen(labeledPicture==ii) = colors(ii+1,2);
-    colorBlue(labeledPicture==ii) = colors(ii+1,3);
-end
-colorImage = cat(3,colorRed,colorGreen,colorBlue);
-
-
-%% Plotting
-if plotOn == true
-    
-    % NDVI mesh
-    % figure(1)
-    % mesh(NDVI)
-    
-    % NDVI image
-    figure(2)
-    imshow(CutNDVI)
-    title(['See all plants, NDVI= ',num2str(cutoffNDVI)])
-    
-    % Labeled & colored image
-    figure(3)
-    imshow(colorImage)
-    title('Labeled regions colored')
-    L = line(ones(length(classLabel)),ones(length(classLabel)), 'LineWidth',2);
-    set(L,{'color'},mat2cell(colors./255,ones(1,length(classLabel)),3));
-    legend(classNames,'Location','southeast');
-    
-end
 
 %% Function testing
 
@@ -212,6 +102,50 @@ end
 
 %% Functions
 
+function plotColorImage(figureNumber,colorImage,classNames,classLabel,colors)
+
+    figure(figureNumber)
+    imshow(colorImage)
+    title('Labeled regions colored')
+    L = line(ones(length(classLabel)),ones(length(classLabel)), 'LineWidth',2);
+    set(L,{'color'},mat2cell(colors./255,ones(1,length(classLabel)),3));
+    legend(classNames,'Location','southeast');
+end
+function colorImage = fillColors(basePicture,labeledPicture,colors)
+
+colorRed = basePicture;
+colorGreen= basePicture;
+colorBlue = basePicture;
+for ii= 1:8
+    colorRed(labeledPicture==ii) = colors(ii+1,1);
+    colorGreen(labeledPicture==ii) = colors(ii+1,2);
+    colorBlue(labeledPicture==ii) = colors(ii+1,3);
+end
+colorImage = cat(3,colorRed,colorGreen,colorBlue);
+
+end
+
+function outPicture =labelPicture(inPicture,edges,label,classLabel,cutoffNDVI,NDVI)
+outPicture = inPicture;
+for ii=1:size(inPicture,1)
+    for jj=1:size(inPicture,2)
+        if strcmp(label,'Soil') || strcmp(label,'Background') || strcmp(label,'Road')
+            if NDVI(ii,jj) < cutoffNDVI
+                if isInsidePolygon([jj,ii],edges) == true
+                    outPicture(ii,jj)=classLabel(label);
+                end
+            end
+        else
+            if NDVI(ii,jj) >= cutoffNDVI
+                if isInsidePolygon([jj,ii],edges) == true
+                    outPicture(ii,jj)=classLabel(label);
+                end
+            end
+        end
+    end
+end
+
+end
 function x = isInsidePolygon(point, edges)
 if size(edges,2)~= 2 || size(edges,1)< 3
     x=false;
@@ -235,5 +169,47 @@ function x = isLeftOfLine(point, linePoints)
         return
     end
     x=true;
+end
+
+function choice = choosedialog(choosableParameters)
+
+    d = dialog('Position',[300 300 250 150],'Name','Select label');
+    
+    txt = uicontrol('Parent',d,...
+           'Style','text',...
+           'Position',[5 80 190 40],...
+           'String','Select the label you wish to add:');
+       
+    popup = uicontrol('Parent',d,...
+           'Style','popup',...
+           'Position',[20 60 200 25],...
+           'String',choosableParameters,...
+           'Callback',@popup_callback);
+       
+    btn = uicontrol('Parent',d,...
+           'Position',[20 20 60 25],...
+           'String','Accept',...
+           'Callback','delete(gcf)');
+       
+    btn = uicontrol('Parent',d,...
+           'Position',[140 20 80 25],...
+           'String','Stop and save',...
+           'Callback',@button_callback);
+       
+    choice = 'done';
+       
+    % Wait for d to close before running to completion
+    uiwait(d);
+    
+    function popup_callback(popup,event)
+        idx = popup.Value;
+        popup_items = popup.String;
+        choice = char(popup_items(idx,:));
+    end
+
+    function button_callback(src,event)
+        choice = 'done';
+        delete(gcf);
+    end
 end
 
