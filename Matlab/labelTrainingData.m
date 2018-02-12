688cb01a7bfb128aa82469d3a19057800a377937
Jannic Veith <jveith@ethz.ch>
2018-02-12 13:45:27 +0100

    precommit 120218

--- Matlab/labelTrainingData.m
+++ Matlab/labelTrainingData.m
@@ -4,30 +4,34 @@
 % ETH Zürich
 %
 %% Method description
-%
+% 
 % This script labels all points with NDVI >= cutoffNDVI with labels 0 to 8.
-% For Soil, Road and Background the condition is NDVI < cutoffNDVI. 
+% For Soil, Road and Background the condition is NDVI < cutoffNDVI.
 % See Labeling for more info.
-%
-% First select the desired label in the dropdown menu, then choose the 
-% limiting corners in COUNTER-CLOCKWISE direction. Confirm selection of 
+% 
+% First select the desired label in the dropdown menu, then choose the
+% limiting corners in COUNTER-CLOCKWISE direction. Confirm selection of
 % points with ENTER,remove last selected point with BACKSPACE.
 % 
 % Input orthomosaic paths are pathBand1 and pathBand8.
 % The labeled image will be saved to the path savePath as a .mat file.
-% 
+%
 
 %% Set options
 clc
 clear all;
 close all;
 
+inPathNIR = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/Orthomosaics/';
+inPathVIS = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/VIS_Orthomosaics/';
+
 % Paths for orthomosaics for Band1 (700nm) and Band8 (803nm)
 pathBand1 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/Orthomosaics/Band1.png';
 pathBand8 = '/Volumes/mac_jannic_2017/thanujan/Datasets/Ximea_Tamron/20170510/Orthomosaics/Band8.png';
 
 % Path to save the labeled data as .mat file
-savePath = '/Volumes/mac_jannic_2017/thanujan/Datasets/labeledOrthomosaics/XimeaT20170510.mat';
+saveDataPath = '/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/Data/';
+saveLabelPath = '/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/Labels/XimeaT20170510.mat';
 
 % Cutoff NDVI for best separation of plants and soil. If you want to see
 % the NDVI image for point selection, set cutoffON to false. The data
@@ -35,9 +39,12 @@ savePath = '/Volumes/mac_jannic_2017/thanujan/Datasets/labeledOrthomosaics/Ximea
 cutoffNDVI = .505;
 cutoffON = false;
 
+% adjustData=True: For all unlabeled pixels set image pixel intensities to 0
+adjustData = false;
+
 
 %% Labeling
-% 
+%
 % Background, unlabeled 0
 % Corn 1
 % Sugarbeet 2
@@ -83,7 +90,7 @@ while 1
     plotColorImage(1,colorPicture,classNames,classLabel,colors)
     label = choosedialog(classNames);
     if strcmp(label, 'done' )
-        break; 
+        break;
     end
     figure(2)
     [edges(:,1),edges(:,2),~] = impixel(colorPicture);
@@ -94,22 +101,46 @@ if ishandle(2)
     close(2);
 end
 
-save(savePath,'labeledPicture');
+save(saveLabelPath,'labeledPicture');
+
+% Modifies the images according to labeledPicture
+if adjustData
+    adjustImageData(inPathNIR,inPathVIS,saveDataPath,labeledPicture);
+end
+
 
 %% Function testing
 
 % isInsidePolygon([361,667],edgesCorn)
 
+
 %% Functions
+
+function adjustImageData(inPathNIR,inPathVIS,saveDataPath,labeledPicture)
+
+for i=1:25
+    temp = imread([inPathNIR,'Band',num2str(i),'.png']);
+    temp( labeledPicture==0 ) =0;
+    imwrite(temp,[saveDataPath,'Band',num2str(i),'.png']);
+    clear temp
+    if i<17
+        temp = imread([inPathVIS,'Band',num2str(i),'.png']);
+        temp( labeledPicture==0 ) =0;
+        imwrite(temp,[saveDataPath,'Band',num2str(i+25),'.png']);
+        clear temp
+    end
+end
+
+end
 
 function plotColorImage(figureNumber,colorImage,classNames,classLabel,colors)
 
-    figure(figureNumber)
-    imshow(colorImage)
-    title('Labeled regions colored')
-    L = line(ones(length(classLabel)),ones(length(classLabel)), 'LineWidth',2);
-    set(L,{'color'},mat2cell(colors./255,ones(1,length(classLabel)),3));
-    legend(classNames,'Location','southeast');
+figure(figureNumber)
+imshow(colorImage)
+title('Labeled regions colored')
+L = line(ones(length(classLabel)),ones(length(classLabel)), 'LineWidth',2);
+set(L,{'color'},mat2cell(colors./255,ones(1,length(classLabel)),3));
+legend(classNames,'Location','southeast');
 end
 function colorImage = fillColors(basePicture,labeledPicture,colors)
 
@@ -162,45 +193,45 @@ x=true;
 end
 
 function x = isLeftOfLine(point, linePoints)
-    grad = diff(linePoints);
-    D = -grad(2) * ( point(1) - linePoints(1,1) ) + grad(1) * ( point(2) - linePoints(1,2) );
-    if(D > 0)
-        x=false;
-        return
-    end
-    x=true;
+grad = diff(linePoints);
+D = -grad(2) * ( point(1) - linePoints(1,1) ) + grad(1) * ( point(2) - linePoints(1,2) );
+if(D > 0)
+    x=false;
+    return
+end
+x=true;
 end
 
 function choice = choosedialog(choosableParameters)
 
-    d = dialog('Position',[300 300 250 150],'Name','Select label');
-    
-    txt = uicontrol('Parent',d,...
-           'Style','text',...
-           'Position',[5 80 190 40],...
-           'String','Select the label you wish to add:');
-       
-    popup = uicontrol('Parent',d,...
-           'Style','popup',...
-           'Position',[20 60 200 25],...
-           'String',choosableParameters,...
-           'Callback',@popup_callback);
-       
-    btn = uicontrol('Parent',d,...
-           'Position',[20 20 60 25],...
-           'String','Accept',...
-           'Callback','delete(gcf)');
-       
-    btn = uicontrol('Parent',d,...
-           'Position',[140 20 80 25],...
-           'String','Stop and save',...
-           'Callback',@button_callback);
-       
-    choice = 'done';
-       
-    % Wait for d to close before running to completion
-    uiwait(d);
-    
+d = dialog('Position',[300 300 250 150],'Name','Select label');
+
+txt = uicontrol('Parent',d,...
+    'Style','text',...
+    'Position',[5 80 190 40],...
+    'String','Select the label you wish to add:');
+
+popup = uicontrol('Parent',d,...
+    'Style','popup',...
+    'Position',[20 60 200 25],...
+    'String',choosableParameters,...
+    'Callback',@popup_callback);
+
+btn = uicontrol('Parent',d,...
+    'Position',[20 20 60 25],...
+    'String','Accept',...
+    'Callback','delete(gcf)');
+
+btn = uicontrol('Parent',d,...
+    'Position',[140 20 80 25],...
+    'String','Stop and save',...
+    'Callback',@button_callback);
+
+choice = 'done';
+
+% Wait for d to close before running to completion
+uiwait(d);
+
     function popup_callback(popup,event)
         idx = popup.Value;
         popup_items = popup.String;
