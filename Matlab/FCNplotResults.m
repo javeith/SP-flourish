bf28f69a4e08612cbd649f43e71ee67f99f85406
Jannic Veith <jveith@ethz.ch>
2018-02-26 15:31:06 +0100

    Created UNET

--- Matlab/FCNplotResults.m
+++ Matlab/FCNplotResults.m
@@ -4,13 +4,19 @@ labelPath1 = [ basePath, 'FIP_20170531.mat'];
 labelPath2 = [ basePath, 'FIP_20170622.mat'];
 labelPath3 = [ basePath, 'FIP_20170802.mat'];
 labelPath4 = [ basePath, 'XimeaT_20170613.mat'];
+labelPath5 = [ basePath, 'XimeaT_20170510.mat'];
+labelPath6 = [ basePath, 'XimeaT_20170622.mat'];
 
 FCNdisplayLabels(labelPath1,'FIP_20170531');
 FCNdisplayLabels(labelPath2,'FIP_20170622');
 FCNdisplayLabels(labelPath3,'FIP_20170802');
 FCNdisplayLabels(labelPath4,'XimeaT_20170613');
+FCNdisplayLabels(labelPath5, 'XimeaT_20170510.mat');
+FCNdisplayLabels(labelPath6, 'XimeaT_20170622.mat');
 
 saveas(1,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aFIP_20170531.png')
 saveas(2,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aFIP_20170622.png')
 saveas(3,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aFIP_20170802.png')
 saveas(4,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aXimeaT_20170613.png')
+saveas(5,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aXimeaT_20170510.png')
+saveas(6,'/Volumes/mac_jannic_2017/thanujan/Datasets/xFcnClassifier/testLabelPredict/aXimeaT_20170622.png')
--- Matlab/classifyDifferentCrops_CNN.m
+++ Matlab/classifyDifferentCrops_CNN.m
@@ -179,122 +179,122 @@ t = [repmat([1,0,0,0,0,0,0,0],size(soil,2),1);repmat([0,1,0,0,0,0,0,0],size(road
 
 size(x)
 size(t)
-% 
-% %% Building the Neural Network Classifier
-% % Since the neural network starts with random initial weights, the results
-% % will differ slightly every time it is run. The random seed
-% % is set to avoid this randomness.
-% 
-% setdemorandstream(491218382)
-% 
-% %% Specify number of hidden layers
-% % Two-layer (i.e. one-hidden-layer) feed forward neural networks can learn
-% % any input-output relationship given enough neurons in the hidden layer.
-% % Layers which are not output layers are called hidden layers.
-% %
-% % We will try a single hidden layer of 10 neurons for this example. In
-% % general, more difficult problems require more neurons, and perhaps more
-% % layers.  Simpler problems require fewer neurons.
-% %
-% % The input and output have sizes of 0 because the network has not yet
-% % been configured to match our input and target data.  This will happen
-% % when the network is trained.
-% 
-% net = patternnet(20);
-% 
-% % Set up Division of Data for Training, Validation, Testing
-% net.divideParam.trainRatio = 70/100;
-% net.divideParam.valRatio = 15/100;
-% net.divideParam.testRatio = 15/100;
-% 
-% % Specify function to be used in hidden layer
-% % net.layers{1}.transferFcn = 'purelin';
-% 
-% view(net)
-% 
-% %% Start training
-% % Now the network is ready to be trained. The samples are automatically
-% % divided into training, validation and test sets. The training set is
-% % used to teach the network. Training continues as long as the network
-% % continues improving on the validation set. The test set provides a
-% % completely independent measure of network accuracy.
-% 
-% [net,tr] = train(net,x,t); %,'useGPU','yes');
-% nntraintool
-% 
-% %% Plot performance
-% % To see how the network's performance improved during training, either
-% % click the "Performance" button in the training tool, or call PLOTPERFORM.
-% %
-% % Performance is measured in terms of mean squared error, and shown in
-% % log scale.  It rapidly decreased as the network was trained.
-% %
-% % Performance is shown for each of the training, validation and test sets.
-% % The version of the network that did best on the validation set is
-% % was after training.
-% 
-% plotperform(tr)
-% set(gca,'fontsize',18);
-% 
-% %% Testing the Classifier
-% % The trained neural network can now be tested with the testing samples
-% % This will give us a sense of how well the network will do when applied
-% % to data from the real world.
-% %
-% % The network outputs will be in the range 0 to 1, so we can use *vec2ind*
-% % function to get the class indices as the position of the highest element
-% % in each output vector.
-% 
-% testX = x(:,tr.testInd);
-% testT = t(:,tr.testInd);
-% 
-% testY = net(testX);
-% testIndices = vec2ind(testY);
-% 
-% %% Confusion Plot
-% % One measure of how well the neural network has fit the data is the
-% % confusion plot.  Here the confusion matrix is plotted across all samples.
-% %
-% % The confusion matrix shows the percentages of correct and incorrect
-% % classifications.  Correct classifications are the green squares on the
-% % matrices diagonal.  Incorrect classifications form the red squares.
-% %
-% % If the network has learned to classify properly, the percentages in the
-% % red squares should be very small, indicating few misclassifications.
-% %
-% % If this is not the case then further training, or training a network
-% % with more hidden neurons, would be advisable.
-% 
-% plotconfusion(testT,testY)
-% set(gca,'fontsize',18);
-% 
-% %% Overall percentages of correct and incorrect classification
-% [corn1_pc,cm] = confusion(testT,testY)
-% 
-% fprintf('Percentage Correct Classification   : %f%%\n', 100*(1-corn1_pc));
-% fprintf('Percentage Incorrect Classification : %f%%\n', 100*corn1_pc);
-% 
-% %% Receiver operating characteristic plot
-% % Another measure of how well the neural network has fit data is the
-% % receiver operating characteristic plot.  This shows how the false
-% % positive and true positive rates relate as the thresholding of outputs
-% % is varied from 0 to 1.
-% %
-% % The farther left and up the line is, the fewer false positives need to
-% % be accepted in order to get a high true positive rate.  The best
-% % classifiers will have a line going from the bottom left corner, to the
-% % top left corner, to the top right corner, or close to that.
-% 
-% plotroc(testT,testY)
-% axisdata = get(gca,'userdata');
-% set(gca,'fontsize',18);
-% legend(axisdata.lines,'Soil', 'Road', 'Buckwheat', 'Corn', 'Grass', 'Soybean', 'Sugarbeet', 'Winter wheat')
-% 
-% %% Precision-Recall curve
-% classes = {'Soil', 'Road', 'Buckwheat', 'Corn', 'Grass', 'Soybean', 'Sugarbeet', 'Winter wheat'};
-% for i = 1:size(testT,1)
-%     prec_rec(testY(i,:),testT(i,:),'holdFigure', 1, 'plotROC', 0, 'plotPR', 1, 'numThresh', 1000, 'plotBaseline', 0);
-% end
-% set(gca,'fontsize',18);
-% legend(classes);
-% grid on
+
+%% Building the Neural Network Classifier
+% Since the neural network starts with random initial weights, the results
+% will differ slightly every time it is run. The random seed
+% is set to avoid this randomness.
+
+setdemorandstream(491218382)
+
+%% Specify number of hidden layers
+% Two-layer (i.e. one-hidden-layer) feed forward neural networks can learn
+% any input-output relationship given enough neurons in the hidden layer.
+% Layers which are not output layers are called hidden layers.
+%
+% We will try a single hidden layer of 10 neurons for this example. In
+% general, more difficult problems require more neurons, and perhaps more
+% layers.  Simpler problems require fewer neurons.
+%
+% The input and output have sizes of 0 because the network has not yet
+% been configured to match our input and target data.  This will happen
+% when the network is trained.
+
+net = patternnet(20);
+
+% Set up Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% Specify function to be used in hidden layer
+% net.layers{1}.transferFcn = 'purelin';
+
+view(net)
+
+%% Start training
+% Now the network is ready to be trained. The samples are automatically
+% divided into training, validation and test sets. The training set is
+% used to teach the network. Training continues as long as the network
+% continues improving on the validation set. The test set provides a
+% completely independent measure of network accuracy.
+
+[net,tr] = train(net,x,t); %,'useGPU','yes');
+nntraintool
+
+%% Plot performance
+% To see how the network's performance improved during training, either
+% click the "Performance" button in the training tool, or call PLOTPERFORM.
+%
+% Performance is measured in terms of mean squared error, and shown in
+% log scale.  It rapidly decreased as the network was trained.
+%
+% Performance is shown for each of the training, validation and test sets.
+% The version of the network that did best on the validation set is
+% was after training.
+
+plotperform(tr)
+set(gca,'fontsize',18);
+
+%% Testing the Classifier
+% The trained neural network can now be tested with the testing samples
+% This will give us a sense of how well the network will do when applied
+% to data from the real world.
+%
+% The network outputs will be in the range 0 to 1, so we can use *vec2ind*
+% function to get the class indices as the position of the highest element
+% in each output vector.
+
+testX = x(:,tr.testInd);
+testT = t(:,tr.testInd);
+
+testY = net(testX);
+testIndices = vec2ind(testY);
+
+%% Confusion Plot
+% One measure of how well the neural network has fit the data is the
+% confusion plot.  Here the confusion matrix is plotted across all samples.
+%
+% The confusion matrix shows the percentages of correct and incorrect
+% classifications.  Correct classifications are the green squares on the
+% matrices diagonal.  Incorrect classifications form the red squares.
+%
+% If the network has learned to classify properly, the percentages in the
+% red squares should be very small, indicating few misclassifications.
+%
+% If this is not the case then further training, or training a network
+% with more hidden neurons, would be advisable.
+
+plotconfusion(testT,testY)
+set(gca,'fontsize',18);
+
+%% Overall percentages of correct and incorrect classification
+[corn1_pc,cm] = confusion(testT,testY)
+
+fprintf('Percentage Correct Classification   : %f%%\n', 100*(1-corn1_pc));
+fprintf('Percentage Incorrect Classification : %f%%\n', 100*corn1_pc);
+
+%% Receiver operating characteristic plot
+% Another measure of how well the neural network has fit data is the
+% receiver operating characteristic plot.  This shows how the false
+% positive and true positive rates relate as the thresholding of outputs
+% is varied from 0 to 1.
+%
+% The farther left and up the line is, the fewer false positives need to
+% be accepted in order to get a high true positive rate.  The best
+% classifiers will have a line going from the bottom left corner, to the
+% top left corner, to the top right corner, or close to that.
+
+plotroc(testT,testY)
+axisdata = get(gca,'userdata');
+set(gca,'fontsize',18);
+legend(axisdata.lines,'Soil', 'Road', 'Buckwheat', 'Corn', 'Grass', 'Soybean', 'Sugarbeet', 'Winter wheat')
+
+%% Precision-Recall curve
+classes = {'Soil', 'Road', 'Buckwheat', 'Corn', 'Grass', 'Soybean', 'Sugarbeet', 'Winter wheat'};
+for i = 1:size(testT,1)
+    prec_rec(testY(i,:),testT(i,:),'holdFigure', 1, 'plotROC', 0, 'plotPR', 1, 'numThresh', 1000, 'plotBaseline', 0);
+end
+set(gca,'fontsize',18);
+legend(classes);
+grid on
